ab269ad8c3b03a6443b90ad21f795790e23c7345
Paolo-T <info@paolotodde.com>
2020-05-08 23:51:09 +0100

    roverPhotos fetch setup

--- src/components/RoverPhotos.js
+++ src/components/RoverPhotos.js
@@ -3,10 +3,15 @@ import useFetch from "../hooks/useFetch";
 import Loader from "./base/Loader";
 
 function RoverPhotos() {
-    const res = useFetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=24TE7EgNfmXIvdb6vNNZGBWx8s54XbZzCCi2oAdN", {});
+    const randomSol = Math.floor(Math.random() * 1001);
+    console.log(randomSol);
 
+    const res = useFetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${randomSol}&api_key=24TE7EgNfmXIvdb6vNNZGBWx8s54XbZzCCi2oAdN`,
+        {}
+    );
 
-    // console.log("Media fetched! --->>>", res.response);
+    console.log("Media fetched! --->>>", res.response);
 
     if (!res.response) {
         return (
@@ -16,25 +21,24 @@ function RoverPhotos() {
         );
     }
 
-	// const photos = res.response;
-	const photos = res.response.photos.filter(photo => {
-		return photo.rover.cameras[0].name === "NAVCAM;"
-	});
-	console.log(photos);
+    const photos = res.response.photos.filter((camera) =>
+        camera.camera.name.includes("NAVCAM")
+    );
+    console.log(photos);
 
-	return(
-		<div className="container mx-auto md:grid gap-6 md:grid-cols-3 rounded-md">
-			{photos.map((photo) => {
-				return (
-					<img
-						className="w-full rounded-sm cursor-pointer"
-						src={photo.img_src}
-						key={photo.id}
-						alt="Scene from Mars Rover"
-					/>
-				)
-			})}
-		</div>
-	);
+    return (
+        <div className="container mx-auto md:grid gap-6 md:grid-cols-3 rounded-md">
+            {photos.map((photo) => {
+                return (
+                    <img
+                        className="w-full rounded-sm cursor-pointer"
+                        key={photo.id}
+                        src={photo.img_src}
+                        alt="Scene from Mars Rover"
+                    />
+                );
+            })}
+        </div>
+    );
 }
 export default RoverPhotos;
