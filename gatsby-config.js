9799bffc98cdb2131d58397ec4e1d1a1de4091e0
Paolo-T <info@paolotodde.com>
2020-05-14 09:29:01 +0100

    Add analitycs

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,39 +4,46 @@ const tailwindConfig = require("./tailwind.config.js");
 const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
-  siteMetadata: {
-    title: `spacedigest.live`,
-    description: `For space aficionados`,
-    author: ``,
-  },
-  plugins: [
-    `gatsby-plugin-eslint`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `spacedigest.live`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: fullConfig.theme.colors.white,
-        theme_color: fullConfig.theme.colors.teal["400"],
-        display: `minimal-ui`,
-        icon: `src/images/favicon.ico`,
-      },
+    siteMetadata: {
+        title: `spacedigest.live`,
+        description: `For space aficionados`,
+        author: ``,
     },
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [
-          require(`tailwindcss`)(tailwindConfig),
-          require(`autoprefixer`),
-          ...(process.env.NODE_ENV === `production`
-            ? [require(`cssnano`)]
-            : []),
-        ],
-      },
-    },
-    `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+                trackingId: "UA-80068675-2",
+            },
+        },
+        `gatsby-plugin-eslint`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `spacedigest.live`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: fullConfig.theme.colors.white,
+                theme_color: fullConfig.theme.colors.teal["400"],
+                display: `minimal-ui`,
+                icon: `src/images/favicon.ico`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-postcss`,
+            options: {
+                postCssPlugins: [
+                    require(`tailwindcss`)(tailwindConfig),
+                    require(`autoprefixer`),
+                    ...(process.env.NODE_ENV === `production`
+                        ? [require(`cssnano`)]
+                        : []),
+                ],
+            },
+        },
+        `gatsby-plugin-offline`,
+    ],
 };
