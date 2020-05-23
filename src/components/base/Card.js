69548d82e382911787eaafce5eeba20beb179938
Paolo-T <info@paolotodde.com>
2020-05-23 22:52:20 +0100

    Update

--- src/components/base/Card.js
+++ src/components/base/Card.js
@@ -5,32 +5,31 @@ function Card({ image, link, key, title, tag1, tag2, tag3 }) {
     return (
         <div
             key={key}
-            className="max-w-sm bg-gray-100 rounded overflow-hidden border-2 hover:border-primary text-center"
+            className="max-w-sm h-full bg-gray-100 flex justify-between rounded overflow-hidden border-2 hover:border-primary text-center"
         >
             <Link to={link}>
+                <img
+                    className="w-full py-12 px-12"
+                    effect="blur"
+                    src={image}
+                    alt="Sunset in the mountains"
+                />
                 <div>
-                    <img
-                        className="w-full py-12 px-12"
-                        effect="blur"
-                        src={image}
-                        alt="Sunset in the mountains"
-                    />
-
-                    <p className="font-bold text-2xl mb-2 text-gray-700 pb-4">
+                    <p className="font-bold text-2xl mb-2 text-gray-700 pb-4 px-8">
                         {title}
                     </p>
-                </div>
 
-                <div className="flex h-16 justify-between bg-gray-200 px-6 py-4">
-                    <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                        Launch {tag1}
-                    </span>
-                    <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700">
-                        {tag2}
-                    </span>
-                    <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-                        {tag3}
-                    </span>
+                    <div className="flex h-16 justify-between bg-gray-200 px-6 py-4">
+                        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                            Launch {tag1}
+                        </span>
+                        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700">
+                            {tag2}
+                        </span>
+                        <span className="inline-block px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+                            {tag3}
+                        </span>
+                    </div>
                 </div>
             </Link>
         </div>
