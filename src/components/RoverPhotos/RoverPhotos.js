91ac8dfde6fc5f28c5658448d44ecca5af0fac33
Paolo-T <info@paolotodde.com>
2020-05-29 21:19:41 +0100

    Update

--- src/components/RoverPhotos/RoverPhotos.js
+++ src/components/RoverPhotos/RoverPhotos.js
@@ -5,7 +5,6 @@ import Moment from "react-moment";
 import chunkArray from "../../utils/chunkArray";
 import Pagination from "../base/Pagination";
 import Photos from "./Photos";
-import Button from "../base/Button";
 
 const PHOTOS_PER_PAGE = 15;
 const START_ON_PAGE_NUMBER = 1;
@@ -36,20 +35,44 @@ function RoverPhotos() {
         }
     }, [res.response]);
 
-    console.log("Photos to display ->", photosToDisplay);
-
     function renderPhotoPage(pageIndex) {
         return <Photos items={photosToDisplay[pageIndex]} key={pageIndex} />;
     }
 
     const changePage = (pageNumber) => setCurrentPage(pageNumber);
 
-    function refreshPage() {
-        window.location.reload(false);
-    }
+    const photoData = JSON.parse(JSON.stringify(res.response));
+
+    console.log("photoData", photoData.photos[0]);
 
     return (
         <>
+            <div className="grid grid-cols-3 gap-4 bg-secondary px-6 py-6 rounded-md mb-6 mt-12">
+                <h4>Rover name: {photoData.photos[0].rover.name}</h4>
+                <h4>Mars Sol: {photoData.photos[0].sol}</h4>
+                <h4>
+                    Earth Date:
+                    <Moment format="DD/MM/YYYY">
+                        {photoData.photos[0].earth_date}
+                    </Moment>
+                </h4>
+
+                <h4>
+                    Launch Date:
+                    <Moment format="DD/MM/YY">
+                        {photoData.photos[0].rover.launch_date}
+                    </Moment>
+                </h4>
+
+                <h4>
+                    Landing Date:
+                    <Moment format="DD/MM/YY">
+                        {photoData.photos[0].rover.landing_date}
+                    </Moment>
+                </h4>
+                <h4>Status: {photoData.photos[0].rover.status}</h4>
+            </div>
+
             {photosToDisplay ? (
                 <div className="container">
                     <Pagination
